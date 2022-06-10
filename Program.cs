4b0965f7fdede58e7392bac60d81c35384494ac5
Sisir Patro <79246986+sispatro@users.noreply.github.com>
2022-06-10 15:37:14 +0530

    change for cpu utilization

--- Program.cs
+++ Program.cs
@@ -1,8 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-for(int i = 0; i < 10000; i++)
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Management;
+namespace HighCPUUsage
 {
-    Console.WriteLine(" Looped as " + i.ToString());
-    Thread.Sleep(1000);
+    class Program
+    {
+        private const string QueryOrClassName = "SELECT LoadPercentage FROM Win32_Processor";
+
+        static void Main(string[] args)
+        {
+            Debugger.NotifyOfCrossThreadDependency(); // for debugging
+            int threadCount = Environment.ProcessorCount;
+            Console.WriteLine("High CPU Usage application.Press ctrl / c for interrupt!");
+            if (args.Length >= 1)
+            {
+                threadCount = Int32.Parse(args[0]);
+            }
+            for (int ii = 0; ii < threadCount; ii++)
+            {
+                Thread thread1 = new(new ThreadStart(ThreadFunction));
+                thread1.Start();
+            }
+            Console.Write("Number of threads: ", threadCount);
+#pragma warning disable CA1416 // Validate platform compatibility
+            SelectQuery cpuQuery = new(QueryOrClassName);
+#pragma warning restore CA1416 // Validate platform compatibility
+            Console.Write("Cpu usage: ");
+            int x = Console.CursorLeft;
+            int y = Console.CursorTop;
+            while (true)
+            {
+                ManagementObjectSearcher mgmtObjSrchr = new ManagementObjectSearcher(cpuQuery);
+                foreach (ManagementObject cpuLoad in mgmtObjSrchr.Get())
+                {
+                    string cpuPercent = cpuLoad.GetPropertyValue("LoadPercentage").ToString();
+                    Console.CursorLeft = x;
+                    Console.CursorTop = y;
+                    Console.Write("{0}%", cpuPercent);
+                }
+                Thread.Sleep(1000);
+
+            }
+            static void ThreadFunction()
+            {
+                while (true)
+                {
+                }
+            }
+        }
+    }
 }
-Console.WriteLine("Good Bye!");
